e54c58aa8e9a7938915be73d8dd5aeba75349c57
Joohee Kim <mungmungtei@gmail.com>
2022-10-19 13:10:44 +0900

    Day 1

---
+++ programmers/Lv.0/두 수의 합.js
@@ -0,0 +1,9 @@
+// 정수 num1과 num2가 주어질 때,
+// num1과 num2의 합을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(solution(2, 3)); // 5
+console.log(solution(100, 2)); // 102
---
+++ programmers/Lv.0/몫 구하기.js
@@ -0,0 +1,7 @@
+// 정수 num1, num2가 매개변수로 주어질 때,
+// num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+const solution = (num1, num2) => Math.floor(num1 / num2);
+
+console.log(solution(10, 5)); // 2
+console.log(solution(7, 2)); // 3

dc881412308aa5f86ef564b5a74170ca4ff57223
Joohee Kim <mungmungtei@gmail.com>
2022-10-27 23:52:27 +0900

    별찍기 연습

---
+++ star-prac.js
@@ -0,0 +1,51 @@
+for (let i = 1; i <= 5; i++) {
+  console.log("*".repeat(i));
+}
+// *
+// **
+// ***
+// ****
+// *****
+
+for (let i = 5; i > 0; i--) {
+  console.log("*".repeat(i));
+}
+// *****
+// ****
+// ***
+// **
+// *
+
+for (let i = 1; i <= 5; i++) {
+  console.log(" ".repeat(5 - i) + "*".repeat(i));
+}
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+for (let i = 1; i <= 5; i += 2) {
+  console.log(" ".repeat((5 - i) / 2) + "*".repeat(i));
+}
+//   *
+//  ***
+// *****
+
+for (let i = 4; i >= -4; i -= 2) {
+  console.log("*".repeat(5 - Math.abs(i)));
+}
+// *
+// ***
+// *****
+// ***
+// *
+
+for (let i = 4; i >= -4; i -= 2) {
+  console.log(" ".repeat(Math.abs(i) / 2) + "*".repeat(5 - Math.abs(i)));
+}
+//   *
+//  ***
+// *****
+//  ***
+//   *
